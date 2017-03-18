620cd466363150b1abb30b5dfe6fd06e65cc0de5
pavanganti <pavanganti@gmail.com>
2017-03-18 18:47:46 -0400

    updated chef cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,17 +4,28 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-include_recipe "apt"
-include_recipe "java"
+#wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key
+remote_file '/tmp/pkg.jenkins.io.key' do
+  source 'https://pkg.jenkins.io/debian/jenkins-ci.org.key'
+  notifies :run, 'execute[apt-key add /tmp/pkg.jenkins.io.key]', :immediately
 
-apt_repository "jenkins" do
-  uri "http://pkg.jenkins-ci.org/debian"
-  key "http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key"
-  components ["binary/"]
-  action :add
 end
 
-package "jenkins"
+#| sudo apt-key add -
+execute 'apt-key add /tmp/pkg.jenkins.io.key' do
+  action :nothing
+end
+#sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+file '/etc/apt/sources.list.d/jenkins.list' do
+  content 'deb http://pkg.jenkins.io/debian-stable binary/'
+  notifies :run, 'execute[apt-get update]', :immediately
+end
+#sudo apt-get update
+execute 'apt-get update' do
+  action :nothing
+end
+
+package 'jenkins'
 
 service "jenkins" do
   supports [:stop, :start, :restart]
