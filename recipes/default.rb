ae472afeee625feaccd9e49dd27b390f0335548d
pavanganti <pavanganti@gmail.com>
2017-03-18 18:09:13 -0400

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,18 +4,19 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-include_recipe 'apt::default'
-include_recipe 'java'
+include_recipe "apt"
+include_recipe "java"
 
-apt_repository 'jenkins' do
-  uri  'http://pkg.jenkins.io/debian-stable'
-  distribution 'binary/'
-  key  'https://pkg.jenkins.io/debian/jenkins-ci.org.key'
-
+apt_repository "jenkins" do
+  uri "http://pkg.jenkins-ci.org/debian"
+  key "http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key"
+  components ["binary/"]
+  action :add
 end
 
-package 'jenkins' do
-  action :remove
-end
+package "jenkins"
 
-package 'jenkins'
+service "jenkins" do
+  supports [:stop, :start, :restart]
+  action [:start, :enable]
+end
